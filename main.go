a54254ff4ae070f3d972a346ccdb81b93dc79697
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2025-07-11 10:51:20 +0700

    initiate server

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"bintaro-university-admission/pages"
+
+	"github.com/a-h/templ"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	index := pages.Index("Dejan")
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		templ.Handler(index).ServeHTTP(w, r)
+	})
+
+	server := http.Server{
+		Handler:      r,
+		Addr:         ":9000",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+	_ = server.ListenAndServe()
+}
